15b274eb9d58e3d9431314fb008741aa237a39b2
Matheus Rocha <matt.lebrao@live.nl>
2023-06-08 14:47:17 -0300

    Added Honda HR-V Touring 2023 Fingerprint

--- selfdrive/car/honda/values.py
+++ selfdrive/car/honda/values.py
@@ -88,6 +88,7 @@ class CAR:
   FREED = "HONDA FREED 2020"
   HRV = "HONDA HRV 2019"
   HRV_3G = "HONDA HR-V 2023"
+  HRV_TOURING_23 = "HONDA HR-V TOURING 2023"
   ODYSSEY = "HONDA ODYSSEY 2018"
   ODYSSEY_CHN = "HONDA ODYSSEY CHN 2019"
   ACURA_RDX = "ACURA RDX 2018"
@@ -140,6 +141,7 @@ CAR_INFO: Dict[str, Optional[Union[HondaCarInfo, List[HondaCarInfo]]]] = {
   CAR.FREED: HondaCarInfo("Honda Freed 2020", min_steer_speed=12. * CV.MPH_TO_MS),
   CAR.HRV: HondaCarInfo("Honda HR-V 2019-22", min_steer_speed=12. * CV.MPH_TO_MS),
   CAR.HRV_3G: HondaCarInfo("Honda HR-V 2023", "All"),
+  CAR.HRV_TOURING_23: HondaCarInfo("Honda HR-V Touring 2023", "All"),
   CAR.ODYSSEY: HondaCarInfo("Honda Odyssey 2018-20"),
   CAR.ODYSSEY_CHN: None,  # Chinese version of Odyssey
   CAR.ACURA_RDX: HondaCarInfo("Acura RDX 2016-18", "AcuraWatch Plus", min_steer_speed=12. * CV.MPH_TO_MS),
@@ -1430,52 +1432,70 @@ FW_VERSIONS = {
   },
   CAR.HRV_3G: {
     (Ecu.eps, 0x18DA30F1, None): [
-      b'\r AP2I232807D                  ',
-      b'39990-3M0-G110\x00\x00',
       b'39990-3W0-A030\x00\x00',
     ],
     (Ecu.gateway, 0x18DAEFF1, None): [
-    # b'\x0bK4585109128\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'38897-3M0-T610\x00\x00',
       b'38897-3W1-A010\x00\x00',
     ],
     (Ecu.srs, 0x18DA53F1, None): [
-    # b'\x10\x0c0812300587NG   \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'77959-3M0-K910\x00\x00',
       b'77959-3V0-A820\x00\x00',
     ],
     (Ecu.combinationMeter, 0x18DA60F1, None): [
-    # b'\x18P1040123\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'78109-3N0-M110\x00\x00',
       b'78108-3V1-A220\x00\x00',
     ],
     (Ecu.vsa, 0x18DA28F1, None): [
-    # b'\x0b30191645272\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'57114-3M6-M010\x00\x00',
       b'57114-3W0-A040\x00\x00',
     ],
     (Ecu.transmission, 0x18DA1EF1, None): [
-    # b'\x0b00033085412\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'28101-6JH-M010\x00\x00',
       b'28101-6EH-A010\x00\x00',
     ],
     (Ecu.programmedFuelInjection, 0x18DA10F1, None): [
-    # b'\x0bL6431102520\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'37805-6HG-M630\x00\x00',
       b'37805-6CT-A710\x00\x00',
     ],
     (Ecu.electricBrakeBooster, 0x18DA2BF1, None): [
-    # b'\x08N2520097\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
-      b'39494-T20-A010\x00\x00',
       b'46114-3W0-A020\x00\x00',
     ],
-    (Ecu.fwdRadar, 0x18DAB0F1, None): [
-    # b'\x18EA91230FFFF0122348000473\x00\x00\x00\x00\x00\x00',
-    ],
-    #(Ecu.unknown, 0x18DAB3F1, None): [
-    # b'36162-T50-E020\x00\x00',
-    #],
   },
+  CAR.HRV_TOURING_23: {
+    (Ecu.eps, 0x18DA30F1, None): [
+      b'\r AP2I232807D                  ',
+      b'39990-3M0-G110\x00\x00',
+    ],
+    (Ecu.gateway, 0x18DAEFF1, None): [
+      b'\x0bK4585109128\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'38897-3M0-T610\x00\x00',
+    ],
+    (Ecu.srs, 0x18DA53F1, None): [
+      b'\x10\x0c0812300587NG   \x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'77959-3M0-K910\x00\x00',
+    ],
+    (Ecu.combinationMeter, 0x18DA60F1, None): [
+      b'\x18P1040123\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'78109-3N0-M110\x00\x00',
+    ],
+    (Ecu.vsa, 0x18DA28F1, None): [
+      b'\x0b30191645272\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'57114-3M6-M010\x00\x00',
+    ],
+    (Ecu.transmission, 0x18DA1EF1, None): [
+      b'\x0b00033085412\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'28101-6JH-M010\x00\x00',
+    ],
+    (Ecu.programmedFuelInjection, 0x18DA10F1, None): [
+      b'\x0bL6431102520\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'37805-6HG-M630\x00\x00',
+    ],
+    (Ecu.electricBrakeBooster, 0x18DA2BF1, None): [
+      b'\x08N2520097\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00',
+      b'39494-T20-A010\x00\x00',
+    ],
+    (Ecu.fwdRadar, 0x18DAB0F1, None): [
+      b'\x18EA91230FFFF0122348000473\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.unknown, 0x18DAB3F1, None): [
+     b'36162-T50-E020\x00\x00',
+    ],
+  }
   CAR.ACURA_ILX: {
     (Ecu.gateway, 0x18daeff1, None): [
       b'38897-TX6-A010\x00\x00',
@@ -1595,6 +1615,7 @@ DBC = {
   CAR.FREED: dbc_dict('honda_fit_ex_2018_can_generated', 'acura_ilx_2016_nidec'),
   CAR.HRV: dbc_dict('honda_fit_ex_2018_can_generated', 'acura_ilx_2016_nidec'),
   CAR.HRV_3G: dbc_dict('honda_civic_ex_2022_can_generated', None),
+  CAR.HRV_TOURING_3G: dbc_dict('honda_civic_ex_2022_can_generated', None),
   CAR.ODYSSEY: dbc_dict('honda_odyssey_exl_2018_generated', 'acura_ilx_2016_nidec'),
   CAR.ODYSSEY_CHN: dbc_dict('honda_odyssey_extreme_edition_2018_china_can_generated', 'acura_ilx_2016_nidec'),
   CAR.PILOT: dbc_dict('acura_ilx_2016_can_generated', 'acura_ilx_2016_nidec'),
@@ -1614,6 +1635,6 @@ HONDA_NIDEC_ALT_PCM_ACCEL = {CAR.ODYSSEY}
 HONDA_NIDEC_ALT_SCM_MESSAGES = {CAR.ACURA_ILX, CAR.ACURA_RDX, CAR.CRV, CAR.CRV_EU, CAR.FIT, CAR.FREED, CAR.HRV, CAR.ODYSSEY_CHN,
                                 CAR.PILOT, CAR.RIDGELINE}
 HONDA_BOSCH = {CAR.ACCORD, CAR.ACCORDH, CAR.CIVIC_BOSCH, CAR.CIVIC_BOSCH_DIESEL, CAR.CRV_5G,
-               CAR.CRV_HYBRID, CAR.INSIGHT, CAR.ACURA_RDX_3G, CAR.HONDA_E, CAR.CIVIC_2022, CAR.HRV_3G}
-HONDA_BOSCH_ALT_BRAKE_SIGNAL = {CAR.ACCORD, CAR.CRV_5G, CAR.ACURA_RDX_3G, CAR.HRV_3G}
+               CAR.CRV_HYBRID, CAR.INSIGHT, CAR.ACURA_RDX_3G, CAR.HONDA_E, CAR.CIVIC_2022, CAR.HRV_3G, CAR.HRV_TOURING_23}
+HONDA_BOSCH_ALT_BRAKE_SIGNAL = {CAR.ACCORD, CAR.CRV_5G, CAR.ACURA_RDX_3G, CAR.HRV_3G, CAR.HRV_TOURING_23}
 HONDA_BOSCH_RADARLESS = {CAR.CIVIC_2022, CAR.HRV_3G}
